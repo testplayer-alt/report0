7d2039356c9e21519e50018cfe1b4a617a896390
testplayer-alt <105909732+testplayer-alt@users.noreply.github.com>
2022-12-09 12:27:12 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ function make(){
     var three = document.getElementById('a3');
     var four = document.getElementById('a4');
     var make = document.getElementById('in');
-    make.innerHTML = "私が今回の単元で学んだことは、" + one.value + "です。<br>特に自分の心に残ったことは、" + two.value + "です。<br>" + two.value + "とは、" + three.value + "です。<br>これに対して私は、" + four.value + "と思いました。<br>次回の単元もこの調子で頑張っていきます、よろしくお願いします。";
+    make.innerHTML = "私が今回の単元で学んだことは、" + one.value + "です。<br>特に自分の心に残ったことは、" + two.value + "です。<br>" + three.value + "です。<br>これに対して私は、" + four.value + "と思いました。<br>次回の単元もこの調子で頑張っていきます、よろしくお願いします。";
+    console.log("文章を作成しました");
+}
+
+function c() {
+    c1 = document.getElementById('a1').value = "";
+    c2 = document.getElementById('a2').value = "";
+    c3 = document.getElementById('a3').value = "";
+    c4 = document.getElementById('a4').value = "";
+    c5 = document.getElementById('in').innerHTML = '<p id="inn"> <br> <br> <br> </p>';
+    console.log("削除しました");
 }
