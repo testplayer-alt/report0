35cf58780f0fd0ef96e0c5dfe7d1f5b4e72a9a0a
testplayer-alt <105909732+testplayer-alt@users.noreply.github.com>
2022-12-09 19:22:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function make(){
     var three = document.getElementById('a3');
     var four = document.getElementById('a4');
     var make = document.getElementById('in');
-    make.innerHTML = "私が今回の単元で学んだことは、" + one.value + "です。<br>特に自分の心に残ったことは、" + two.value + "です。<br>" + three.value + "です。<br>これに対して私は、" + four.value + "と思いました。<br>次回の単元もこの調子で頑張っていきます、よろしくお願いします。";
+    make.value = "私が今回の単元で学んだことは、" + one.value + "です。\n特に自分の心に残ったことは、" + two.value + "です。\n" + three.value + "です。\nこれに対して私は、" + four.value + "と思いました。\n次回の単元もこの調子で頑張っていきます、よろしくお願いします。";
     console.log("文章を作成しました");
 }
 
@@ -15,4 +15,12 @@ function c() {
     c4 = document.getElementById('a4').value = "";
     c5 = document.getElementById('in').innerHTML = '<p id="inn"> <br> <br> <br> </p>';
     console.log("削除しました");
+}
+
+addEventListener('click', value);
+addEventListener('keyup', value);
+function value(){
+    var countnumver = document.getElementById('in').value;
+    console.log(countnumver.length);
+    document.getElementById('length').innerHTML = countnumver.length + "文字";
 }
